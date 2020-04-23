017cb9df83e8f769cdeeae6596bb502a87905c94
Sarah Szymborski <szymbor2@illinois.edu>
2020-04-22 19:51:03 -0500

    Update with column that has precip type

--- datatocsv.py
+++ datatocsv.py
@@ -18,6 +18,19 @@ for i in range(len(df)):
         temp = copy.deepcopy(df['Temperature'].iloc[i])
         df['Temperature'].iloc[i] = copy.deepcopy(df['Dewpoint'].iloc[i])
         df['Dewpoint'].iloc[i] = temp
+        
+# Add another column to determine precip type (np.nan for days without, 0 for liquid, 1 for solid
+prcp_type = np.empty((0))
+for i in range(len(df)):
+    print(df.iloc[i])
+    if df['solid'].iloc[i] > 0: # SOLID IS ONE
+        prcp_type = np.append(prcp_type, 1)
+    elif data['liquid'].iloc[i] > 0: # LIQUID IS ZERO
+        prcp_type = np.append(prcp_type, 0)
+    else:
+        prcp_type = np.append(prcp_type, np.nan) # ELSE, NAN
+    break
+df['prcp_type'] = pd.Series(prcp_type, index=df.index)
 
 # Save to CSV file
 df.to_csv(YOUR_DIRECTORY + 'data.csv')
