7dee9353c23b1dce3b2419d52efdf1562440256d
Sarah Szymborski <szymbor2@illinois.edu>
2020-04-22 20:12:45 -0500

    Deleted the break I had accidentally left...

--- datatocsv.py
+++ datatocsv.py
@@ -1,5 +1,6 @@
 # Convert reviewed files into a big CSV file
 # Make sure to set working directory
+
 import pandas as pd
 YOUR_DIRECTORY = '/content/drive/My Drive/Colab Notebooks/ATMS597/project5/'
 
@@ -20,15 +21,14 @@ for i in range(len(df)):
         
 # Add another column to determine precip type (np.nan for days without, 0 for liquid, 1 for solid
 prcp_type = np.zeros((len(df)))
+df['prcp_type'] = pd.Series(prcp_type, index=df.index)
 for i in range(len(df)):
     if df['solid'].iloc[i] > 0: # SOLID IS ONE
-        prcp_type[i] = 1
+        df['prcp_type'].iloc[i] = 1
     elif df['liquid'].iloc[i] > 0: # LIQUID IS ZERO
-        prcp_type[i] = 0
+        df['prcp_type'].iloc[i] = 0
     else: # ELSE, NAN
-        prcp_type[i] = np.nan
-    break
-df['prcp_type'] = pd.Series(prcp_type, index=df.index)
+        df['prcp_type'].iloc[i] = np.nan
 
 # Save to CSV file
 df.to_csv(YOUR_DIRECTORY + 'data.csv')
